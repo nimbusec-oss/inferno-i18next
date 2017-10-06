9f746ef9c1eb4d65a5a773c4bf3d48a48e6092b2
h4ckedneko <h4ckedneko@tfwno.gf>
2017-10-06 16:32:27 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { peerDependencies } from './package.json';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
@@ -7,6 +8,31 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   input: 'src/index.js',
+  external: Object.keys(peerDependencies),
+  globals: {
+    'i18next': 'i18next',
+    'inferno': 'Inferno',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+    commonjs({
+      'namedExports': {
+        'node_modules/inferno-shared/index.js': [
+          'isArray',
+          'isString',
+          'throwError',
+        ],
+      },
+    }),
+    nodeResolve({
+      browser: true,
+      jsnext: true,
+    }),
+    isProduction && uglify(),
+  ],
   output: {
     file: 'dist/bundle' + (isProduction ? '.min.js' : '.js'),
     format: 'umd',
@@ -16,16 +42,4 @@ export default {
   amd: {
     id: 'inferno-i18next',
   },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers'],
-    }),
-    commonjs(),
-    nodeResolve({
-      browser: true,
-      jsnext: true,
-    }),
-    isProduction && uglify(),
-  ],
 };
